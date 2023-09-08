8fa9c79d0098678f9fed4326d263045776d08874
peetah001 <okpelepeter001@gmail.com>
2023-09-08 10:03:48 -0700

    elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,10 +6,13 @@
 #include <unistd.h>
 # include <string.h>
 # include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _strlen(char *str);
 #endif
